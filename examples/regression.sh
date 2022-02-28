a318146d44f274f783fcf037c6951d871d0ec15f
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2022-02-28 00:53:57 -0600

    updated regression

--- examples/regression.sh
+++ examples/regression.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# declare an array called files, that contains 3 values
 files=( "first" "follow" "nested-cycles" "simple-oag" "simple-coag"
         "local-fiber-cycle" "below-fiber-cycle" "below-single-fiber-cycle"
         "broad-fiber-cycle" "simple-binding"
-        "simple-binding1" "simple-binding2" "simple-binding3" )
+        "simple-binding1" "simple-binding2" "simple-binding3"
+        "cool-noinherit-semant" )
 
 for i in "${files[@]}"
 do
   echo "=> Working on $i.aps"
-  ../bin/apssched -p .:../base -DTo $i
+  ../bin/apssched -p .:../base $i
   echo 
 done || exit 0
